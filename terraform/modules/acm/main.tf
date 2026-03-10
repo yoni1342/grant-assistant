5e2cd7ce785df3d0976e3c4d477e66a765f5c34d
Nova4AI <novanavi30@gmail.com>
2026-03-10 07:21:48 +0300

    ci: add terraform/modules/acm/main.tf

---
+++ terraform/modules/acm/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = { source = "hashicorp/aws" }
+  }
+}
+
+variable "domain_name" {}
+variable "route53_zone_id" {}
+
+resource "aws_acm_certificate" "main" {
+  domain_name               = var.domain_name
+  subject_alternative_names = ["www.${var.domain_name}"]
+  validation_method         = "DNS"
+  lifecycle { create_before_destroy = true }
+  tags = { Name = var.domain_name }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.main.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+  zone_id         = var.route53_zone_id
+  name            = each.value.name
+  type            = each.value.type
+  records         = [each.value.record]
+  ttl             = 60
+  allow_overwrite = true
+}
+
+resource "aws_acm_certificate_validation" "main" {
+  certificate_arn         = aws_acm_certificate.main.arn
+  validation_record_fqdns = [for r in aws_route53_record.cert_validation : r.fqdn]
+}
+
+output "certificate_arn" { value = aws_acm_certificate_validation.main.certificate_arn }
